b1e2bcccebafb036beb62fcb3706c97595bf5f92
Heloísa Marquezini <130071994+heloisamarquezini@users.noreply.github.com>
2024-03-26 09:27:02 -0300

    Forca - Parte 1

---
+++ forca.py
@@ -0,0 +1,24 @@
+def jogar():
+    print("*********************************")
+    print(***Bem-vindo ao jogo da Forca!Heloísa
+    print("*********************************")
+    
+    palavra secreta = "Heloísa".upper()
+    letras acertadas = ["_" for letra in palavra secreta]
+    letras_acertadas = []
+    
+    for letra in (palavra secreta):
+        letras_acertadas2.append("_")
+        
+        enforcou = False
+        acertou = False
+        erros = 0
+        
+        print(letra_acertadas)
+        
+        while (not enforcou and not acertou):
+            
+            chute = input("Qual letra?")
+            chute = chute.strip() .upper()
+    
+
