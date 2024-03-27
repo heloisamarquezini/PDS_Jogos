554d7731100adb91e99f9b82d28562e16065ecf9
Heloísa Marquezini <130071994+heloisamarquezini@users.noreply.github.com>
2024-03-27 08:40:01 -0300

    Update adivinhação.py

--- adivinhação.py
+++ adivinhação.py
@@ -1,23 +1,54 @@
-import forca
-import adivinhacao
-
-def escolhe_jogo():
-    print("*********************************")
-    print("*******Escolha o seu jogo!*******")
-    print("*********************************")
+import random
     
-    print("(1) Forca(2) Adivinhação")
+    numeroSecreto = random.randint(1,101)
+    tentavias  = 101
     
-    jogo = int(input("Qual jogo?"))
     
-    if(jogo == 1):
-        print("Jogando forca")
-        froca.jogar()
-    elif(jogo == 2):
-        print(" Jogando adivinhação")
-        adivinhacao.jogar()
+    def jogar():
         
-if(_name_)  == "_main_" :
-    escolhe_jogo()
+        print("*********************************")
+        print("Bem vindo ao jogo de Adivinhação!")
+        print("*********************************")
         
+        NivelDificuldade()
+        
+        print("numero screto" , numeroecreto)
+        global tentativas
+        while tentativas >0:
+            numeroDigitado = int(
+                input(" Digite um número de 1 a 100 para tentar adivinhar o número secreto:"))
+            if nuemeroDigitado == numeroSecreto:
+                print("Parabéns o numero ecolhido esta correto")
+                print("O numero ecreto era" , numeroSecreto)
+                break
+            else:
+                tentativas -= 1        
+                print("O número digitado está errado")
+                print(dica(numeroDigitado))
+                if tentativas > 0;
+                   print("Tente novamente, voce tem:" , tentativas, "tentativas")
+                else:
+                    print("Game Over")
+                    
+                    
+    def dica(numeroEscolhido: int):
+        print("Escolhido", numeroEscolhido, "Secreto, numeroSecreto",numeroEscolhido
+        if numeroEscolhido>numeroSecreto:
+            return "O numero escolhido é Maior que o numero secreto," numeroEscolhido
+       else:
+           return: "O numero escolhido é Menor que o numero secreto" ,numeroEscolhido
+           
+           
+    def NivelDiiculdade():
+        print("Qual o nivel de diiculdade?")
+        print("(1)Fácil(2) Médio (3) Díicil)
+        dificuldade = int(input("Defina o nível"))
+        global tentativas
+        if (dificulade ==1):
+            tentativas = 10
+        elif (difuldade == 2):
+            tentativas = 8
+        else:
+            tentativas = 6
+    jogar()
         
